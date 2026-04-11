757f38595585397fd1f8c666d149f6b4f7ccca70
Leonardo Oliveira <71794931+LeonardoOliveira96@users.noreply.github.com>
2026-04-11 11:54:37 -0300

    feat: add Github Pages Integration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/logplus/" : "/",
+  base: mode === "production" ? "/LogView/" : "/",
   server: {
     host: "::",
     port: 8080,
